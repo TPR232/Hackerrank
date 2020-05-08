b0ffbf858714f3b7bf1fb027eb1c99f0a3ac7966
tirth-hacker <60417384+tirth-hacker@users.noreply.github.com>
2020-05-08 10:59:11 +0530

    Create Day_of_The_Programmer.cpp

---
+++ Day_of_The_Programmer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,a,b;
+    cin>>n;
+    if(n==1918)
+    {
+        cout<<"26.09."<<n;
+    }
+    else if((n<1918 && n%4==0))
+    {
+        cout<<"12.09."<<n;
+    }
+    else if((n%400==0) || ((n%4==0) && (n%100!=0)))
+    {
+        cout<<"12.09."<<n;
+    }
+    else {
+        cout<<"13.09."<<n;
+    }
+}
